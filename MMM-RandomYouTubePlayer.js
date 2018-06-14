3aa8013501b21cf89b97e81ec5ab11d8c755d7c3
JIM HALLOCK <justjim1220@gmail.com>
2018-06-13 19:33:50 -0500

    Update MMM-RandomYouTubePlayer.js
    Updating Changes made to (FINALLY!!!) get a working module

--- MMM-RandomYouTubePlayer.js
+++ MMM-RandomYouTubePlayer.js
@@ -11,125 +11,91 @@
  * NOT tested with Raspberry Pi or Linux-Based systems.
  * It DOES work with Windows 10!!!
  *
- * vs. -- 1.5.0
+ * version: 2.0.0
  *
  * By Jim Hallock (justjim1220@gmail.com)
  *
- * Licensed with a crapload of good ole' Southern Sweet Tea 
+ * a huge, huge THANKS goes out to asimhsidd for his collaboration, hints, and pointers!!!
+ * (Dude, you saved me from some major headaches!!!)
+ *
+ * Licensed with a crapload of good ole' Southern Sweet Tea
  * and a lot of Cheyenne Extreme Menthol cigars!!!
  */
 
 Module.register("MMM-RandomYouTubePlayer", {
 	defaults: {
 		listType: "playlist",
+		// playlist ID is found in the URL on YouTube:
+		// https://www.youtube.com/playlist?list=PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW
+		// whereas "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW" is the 'list' you need to replace below
 		list: "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW",
-		volume: "100", //percentage of starting sound 0-100
-        height: "480", // specified in pixels (px)
-        width: "720", // specified in pixels (px)
+		cc_load_policy: "true", // shows or hides closed captions if available
+		color: "red", // shows status bar color on the player, "red" or "white"
+		controls: "true", // shows or hides the player controls
+		disablekb: "0", // enables or disables keyboard functions
+		enablejsapi: "true", // Enables the player to be controlled via IFrame API calls
+		fs: "false", // enables or disables the fullscreen player
+		iv_load_policy: "1", // shows or hides video annotations
+		modestbranding: "true", // shows or hides the YouTube logo on the player
+		showinfo: "true", // shows or hides the video's information
+		rel: "false", // shows or hides related videos at end of video
+		volume: "100%", // percentage of starting sound 0-100
+		height: "394", // specified in pixels (px)
+		width: "700", // specified in pixels (px)
 		autoplay: "true", // to automatically play when player gets loaded
 		loop: "true" // to replay the playlist continuously
 	},
-	
-	// playlist ID is found in the URL on YouTube: 
-	// https://www.youtube.com/playlist?list=PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW
-	// whereas "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW" is the ID
 
-	/* Other Parameters that can be configured - for full explanation of each,
-		go to https://developers.google.com/youtube/player_parameters#Parameters
-        cc_load_policy: "true",
-		color: "red",
-		controls: "false",
-        disablekb: "true",
-        enablejsapi: "true",
-		fs: "false",
-		iv_load_policy: "3",
-		modestbranding: "false",
-        showinfo: "false",
-        rel: "false",
-    */
-	
+	requiresVersion: "2.2.0",
 
-	getDom: function() { // to override Dom generator
-		// creates the html container for the player
+	start: function(){
+		self = this;
+		self.loaded = false;
+		var el = document.createElement("script");
+		el.src = "//www.youtube.com/iframe_api";
+		el.onload = function(){
+			self.loaded = true;
+			setTimeout( function(){
+				console.log("MMM-RandomYTPlayer: Video wrapper made!");
+				var wrapper = document.getElementById(self.identifier + "_wrapper"),
+					playerDiv = document.createElement("div");
+				wrapper.innerHTML = "";
+				wrapper.className = "";
+				playerDiv.id = "YT_Player";
+				wrapper.appendChild(playerDiv);
+
+				self.player = new YT.Player("YT_Player", {
+					volume: self.config.volume,
+					height: self.config.height,
+					width: self.config.width,
+					autoplay: self.config.autoplay,
+					playerVars:
+					{
+						listType:"playlist",
+						list: self.config.list
+					},
+					loop: self.config.loop,
+					events: {
+						onReady: self.onPlayerReady
+					}
+				});
+			}, 500);
+		};
+		document.querySelector("body").appendChild(el);
+	},
+	getDom: function() {
+		var self = this;
 		var wrapper = document.createElement("div");
-		// loads the IFrame Player API code asynchronously
-		var scriptContainer = document.createElement('script');
-
-		// creates an <iframe> (and YouTube player) after the API code downloads
-    	function onYouTubeIframeAPIReady() {
-			var player = new YT.Player("player", {
-				volume: "100",
-				height: "480",
-				width: "720",
-				autoplay: "true",
-				loop: "true",
-		    	events: {
-				onReady: onPlayerReady,
-				onStateChange: onPlayerStateChange,
-	    		}
-			});
-    	}
-
-		var maxNumber;
-		var oldNumber = 0;
-		var NewNumber = 0;
-
-    	function newRandomNumber() {
-			oldNumber = NewNumber;
-			NewNumber = Math.floor(Math.random() * maxNumber);
-			if (NewNumber == oldNumber) {
-			    newRandomNumber();
-			}
-				else {
-			    	return NewNumber;
-			  	}
-   		}
-
-    	function onPlayerReady(event) {
-			player.loadPlaylist();
-
-			// event.target.setShuffle(true);
-    	}
-
-		var firstLoad = true;
-		var playlistArray;
-		var playListArrayLength;
-
-    	function onPlayerStateChange(event) {
-			console.log(event.data.playlist);
-			if (event.data == YT.PlayerState.ENDED) {
-			    player.playVideoAt(newRandomNumber());
-			} else {
-			if (firstLoad && event.data == YT.PlayerState.PLAYING) {
-			    firstLoad = false;
-
-			playlistArray = player.getPlaylist();
-			playListArrayLength = playlistArray.length;
-			maxNumber = playListArrayLength;
-			NewNumber = newRandomNumber();
-			player.playVideoAt(newRandomNumber());
-			}
-			}
+		wrapper.id = self.identifier + "_wrapper";
+		if (!self.loaded) {
+			wrapper.innerHTML = this.translate("MMM-RandomYouTubePlayer is Loading.");
+			wrapper.className = "dimmed light small";
+			return wrapper;
 		}
-
-		wrapper.appendChild(scriptContainer);
-
-		var tag = document.createElement("script");	
-		tag.src = "https://www.youtube.com/iframe_api";
-		wrapper.appendChild(tag);
-
-		var firstScriptTag = document.getElementsByTagName("script")[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		wrapper.appendChild(firstScriptTag);
-
-		var TempDiv = document.createElement("div");
-		TempDiv.setAttribute(this.config.list, "player");
-		wrapper.appendChild(TempDiv);
-
-        var done = false;
-
-		wrapper.innerHTML = "<iframe ID=player width=\"" + this.config.width +"\" height=\"" + this.config.height + "\" src=\"https://www.youtube.com/embed/?" + this.config.listType + "&" + this.config.list + "frameborder=\"0\"></iframe>";
-
 		return wrapper;
+	},
+	onPlayerReady: function (event) {
+		event.target.setShuffle(true);
+		event.target.playVideo();
 	}
 });
