d710e937a974bd4ac452730e666ee850e3cc5c3f
JIM HALLOCK <justjim1220@gmail.com>
2018-06-22 21:58:03 -0500

    Update MMM-RandomYouTubePlayer.js
    It's FINALLY doing what I wanted it to do!!!

--- MMM-RandomYouTubePlayer.js
+++ MMM-RandomYouTubePlayer.js
@@ -1,4 +1,4 @@
-﻿/* MagicMIrror Module - MMM-RandomYouTubePlayer
+/* MagicMIrror Module - MMM-RandomYouTubePlayer
  *
  * This is a module for the [MagicMirror² By Michael Teeuw http://michaelteeuw.nl]
  * (https://github.com/MichMich/MagicMirror/).
@@ -23,19 +23,19 @@
  */
 
 Module.register("MMM-RandomYouTubePlayer", {
-	defaults: {
-		listType: "playlist",
-		// playlist ID is found in the URL on YouTube:
-		// https://www.youtube.com/playlist?list=PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW
-		// whereas "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW" is the 'list' you need to replace below
-		list: "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW",
-		height: "394", // specified in pixels (px)
-		width: "700", // specified in pixels (px)
-		enablejsapi: "true", // Enables the player to be controlled via IFrame API calls
-		volume: "100%", // percentage of starting sound 0-100
-		autoplay: "true", // to automatically play when player gets loaded
-		loop: "true" // to replay the playlist continuously
-		
+    defaults: {
+        listType: "playlist",
+        // playlist ID is found in the URL on YouTube:
+        // https://www.youtube.com/playlist?list=PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW
+        // whereas "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW" is the 'list' you need to replace below
+        list: "PLl_KM23gznEAZW-INW8ty4QNaHH8JCnNW",
+        height: "394", // specified in pixels (px)
+        width: "700", // specified in pixels (px)
+        enablejsapi: "true", // Enables the player to be controlled via IFrame API calls
+        volume: "100%", // percentage of starting sound 0-100
+        autoplay: "true", // to automatically play when player gets loaded
+        loop: "true" // to replay the playlist continuously
+
 		/* other congiurable options
 		cc_load_policy: "true", // shows or hides closed captions if available
 		color: "red", // shows status bar color on the player, "red" or "white"
@@ -47,58 +47,67 @@ Module.register("MMM-RandomYouTubePlayer", {
 		showinfo: "true", // shows or hides the video's information
 		rel: "false", // shows or hides related videos at end of video
 		*/
-	},
+    },
 
-	requiresVersion: "2.2.0",
+    requiresVersion: "2.1.0",
 
-	start: function(){
-		self = this;
-		self.loaded = false;
-		var el = document.createElement("script");
-		el.src = "https://www.youtube.com/iframe_api";
-		el.onload = function(){
-			self.loaded = true;
-			setTimeout( function(){
-				console.log("MMM-RandomYTPlayer: Video wrapper made!");
-				var wrapper = document.getElementById(self.identifier + "_wrapper"),
-					playerDiv = document.createElement("div");
-				wrapper.innerHTML = "";
-				wrapper.className = "";
-				playerDiv.id = "YT_Player";
-				wrapper.appendChild(playerDiv);
+    start: function () {
 
-				self.player = new YT.Player("YT_Player", {
-					height: self.config.height,
-					width: self.config.width,
-					volume: self.config.volume,
-					autoplay: self.config.autoplay,
-					loop: self.config.loop,
-					playerVars:
-					{
-						listType: "playlist",
-						list: self.config.list
-					},
-					events: {
-						onReady: self.onPlayerReady
-					}
-				});
-			}, 500);
-		};
-		document.querySelector("body").appendChild(el);
-	},
-	getDom: function() {
-		var self = this;
-		var wrapper = document.createElement("div");
-		wrapper.id = self.identifier + "_wrapper";
-		if (!self.loaded) {
-			wrapper.innerHTML = this.translate("MMM-RandomYouTubePlayer is Loading.");
-			wrapper.className = "normal regular small";
-			return wrapper;
-		}
-		return wrapper;
-	},
-	onPlayerReady: function (event) {
-		event.target.setShuffle(true);
-		event.target.playVideo();
-	}
+        self = this;
+        self.loaded = false;
+
+        var el = document.createElement("script");
+        el.src = "https://www.youtube.com/iframe_api";
+
+        el.onload = function () {
+            // self.loadPlaylist(this.config.list);
+            self.loaded = true;
+            setTimeout(function () {
+                console.log("MMM-RandomYTPlayer: Video wrapper made!");
+                var wrapper = document.getElementById(self.identifier + "_wrapper"),
+                    playerDiv = document.createElement("div");
+                wrapper.innerHTML = "";
+                wrapper.className = "";
+                playerDiv.id = "YT_Player";
+                wrapper.appendChild(playerDiv);
+
+                self.player = new YT.Player("YT_Player", {
+                    height: self.config.height,
+                    width: self.config.width,
+                    enablejsapi: self.config.enablejsapi,
+                    playerVars: {
+                        listType: "playlist",
+                        list: self.config.list,
+                    },
+                    events: {
+                        onReady: self.onPlayerReady,
+                    },
+                    volume: self.config.volume,
+                    autoplay: self.config.autoplay,
+                    loop: self.config.loop,
+                });
+            }, 1000);
+        };
+        document.querySelector("body").appendChild(el);
+    },
+    getDom: function() {
+        var self = this;
+        var wrapper = document.createElement("div");
+        wrapper.id = self.identifier + "_wrapper";
+        if (!self.loaded) {
+            wrapper.innerHTML = this.translate("MMM-RandomYouTubePlayer is Loading.");
+            wrapper.className = "normal regular small";
+            return wrapper;
+        }
+        return wrapper;
+    },
+
+    onPlayerReady: function (event) {
+        console.log(event.data);
+        var listArray = self.player.getPlaylist();
+        var arrayLength = listArray.length;
+        event.target.setShuffle(true);
+        var index = Math.floor(Math.random() * arrayLength);
+        event.target.playVideoAt(index);
+    }
 });
